fbe4afc30f64ecec4b901a7a444df9227a45c467
Matheus Felipe <matheus.pinho@globalhitss.com.br>
2024-11-06 19:50:09 -0300

    Adicionar arquivos de projeto.

---
+++ APICatalogo/Program.cs
@@ -0,0 +1,40 @@
+using APICatalogo.Context;
+using APICatalogo.Filters;
+using APICatalogo.Logging;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers()
+      .AddJsonOptions(options =>
+         options.JsonSerializerOptions
+            .ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+string stringConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+                    options.UseSqlServer(stringConnection));
+
+builder.Services.AddScoped<ApiLoggingFilter>();
+
+builder.Logging.AddProvider(new CustomLoggerProvider(new CustomLoggerProviderConfiguration
+{
+    LogLevel = LogLevel.Information
+}));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
